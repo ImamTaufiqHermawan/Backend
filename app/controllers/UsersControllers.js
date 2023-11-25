5d28ea4a6ce52265f58cb0ec2bbbd18698d52b5a
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2023-11-25 14:43:37 +0700

    add functionally admin login

--- app/controllers/UsersControllers.js
+++ app/controllers/UsersControllers.js
@@ -1,12 +1,14 @@
-const Users = require('../models/User');
-const { resSuccess } = require('./resBase');
-const ApiError = require('../utils/apiError');
-const bcrypt = require('bcrypt');
+const Users = require("../models/User");
+const { resSuccess } = require("./resBase");
+const ApiError = require("../utils/apiError");
+const bcrypt = require("bcrypt");
 
 const findAllusers = async (req, res, next) => {
   try {
-    const user = await Users.find();
-    res.status(200).send(resSuccess(user));
+    const user = await Users.find().select(
+      "-password -refreshToken -passwordResetExp -otp -__v",
+    );
+    res.status(200).send(resSuccess("Get all users data sucess", user));
   } catch (error) {
     next(new ApiError(error.message));
   }
@@ -15,11 +17,13 @@ const findAllusers = async (req, res, next) => {
 const userFindByid = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const user = await Users.findById(id);
+    const user = await Users.findById(id).select(
+      "-password -refreshToken -passwordResetExp -otp -__v",
+    );
     if (!user) {
-      return next(new ApiError('User not found', 404));
+      return next(new ApiError("User not found", 404));
     }
-    res.status(200).send(resSuccess(user));
+    res.status(200).send(resSuccess("Get all users data by id", user));
   } catch (error) {
     next(new ApiError(error.message));
   }
@@ -45,13 +49,13 @@ const UserUpdate = async (req, res, next) => {
 
     const user = await Users.findByIdAndUpdate(id, newdata, {
       new: true,
-    }).select('-password');
+    }).select("-password -refreshToken -passwordResetExp -otp -__v");
 
     if (!user) {
-      return next(new ApiError('User not found', 404));
+      return next(new ApiError("User not found", 404));
     }
 
-    res.status(200).send(resSuccess('update sucess', user));
+    res.status(200).send(resSuccess("Update Sucessfully", user));
   } catch (error) {
     console.error(error);
     next(new ApiError(error.message));
@@ -65,17 +69,17 @@ const updatePassword = async (req, res, next) => {
     const user = await Users.findById(id);
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     if (!bcrypt.compareSync(oldpassword, user.password)) {
-      return next(new ApiError('Your old password is incorrect.', 400));
+      return next(new ApiError("Your old password is incorrect.", 400));
     }
 
     if (newpassword !== confirmpassword) {
       return next(
         new ApiError(
-          'New password and new confirm password does not match.',
+          "New password and new confirm password does not match.",
           400,
         ),
       );
@@ -83,15 +87,14 @@ const updatePassword = async (req, res, next) => {
 
     if (newpassword.length < 8) {
       return next(
-        new ApiError('Minimum password length is 8 characters.', 401),
+        new ApiError("Minimum password length is 8 characters.", 401),
       );
     }
 
-    ///hash password
     const hashedPassword = bcrypt.hashSync(newpassword, 10);
     user.password = hashedPassword;
     await user.save();
-    res.status(200).send(resSuccess('Password updated successfully'));
+    res.status(200).send(resSuccess("Password updated successfully"));
   } catch (error) {
     next(new ApiError(error.message));
   }
