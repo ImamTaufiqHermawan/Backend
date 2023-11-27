5c1278e25c990d112cf925a191f9467e00c7861d
mnuralim <shadownur345.com>
2023-11-27 17:28:19 +0800

    fix

--- app/routes/payment.js
+++ app/routes/payment.js
@@ -3,6 +3,6 @@ const paymentController = require("../controllers/payment");
 const authenticate = require("../middleware/authenticate");
 
 route.post("/", authenticate, paymentController.createPayment);
-route.post("/payment-callback", authenticate, paymentController.paymentCallback);
+route.post("/payment-callback", paymentController.paymentCallback);
 
 module.exports = route;
