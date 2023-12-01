c71e28dc391e3555e88f27481e9949ae6334503f
irfiyandaabidin <irfidupang@gmail.com>
2023-12-01 16:32:10 +0700

    add seeder course

--- database/seeders/index.js
+++ database/seeders/index.js
@@ -4,6 +4,8 @@ const User = require("../../app/models/user");
 const mongoose = require("mongoose");
 const Category = require("../../app/models/category");
 const bcrypt = require("bcryptjs");
+const Chapter = require("../../app/models/chapter");
+const Course = require("../../app/models/course");
 
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
@@ -81,6 +83,149 @@ async function createDatabaseAndCollection() {
       },
     ];
 
+    const sampleChapter = [
+      {
+        title: "Chapter 1 - Pendahuluan",
+        totalDuration: 60,
+        videos: [
+          {
+            index: 1,
+            title: "Tujuan Mengikuti Kelas Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/ixOd42SEUF0",
+          },
+          {
+            index: 2,
+            title: "Pengenalan Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/DwTkyMJi890",
+          },
+          {
+            index: 3,
+            title: "Contoh Dalam Membangun Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/rd-590n3H6w",
+          },
+        ],
+      },
+      {
+        title: "Chapter 2 - Memulai Desain",
+        totalDuration: 120,
+        videos: [
+          {
+            index: 4,
+            title: "Color Palette",
+            duration: 60,
+            videoUrl: "https://youtu.be/HYfG_uCOlhc",
+          },
+          {
+            index: 5,
+            title: "Desain Respon",
+            duration: 60,
+            videoUrl: "https://youtu.be/DmxXl1k0X5g",
+          },
+        ],
+      },
+      {
+        title: "Chapter 1 - Pendahuluan",
+        totalDuration: 60,
+        videos: [
+          {
+            index: 1,
+            title: "Tujuan Mengikuti Kelas Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/ixOd42SEUF0",
+          },
+          {
+            index: 2,
+            title: "Pengenalan Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/DwTkyMJi890",
+          },
+          {
+            index: 3,
+            title: "Contoh Dalam Membangun Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/rd-590n3H6w",
+          },
+        ]
+      },
+      {
+        title: "Chapter 2 - Memulai Desain",
+        totalDuration: 120,
+        videos: [
+          {
+            index: 4,
+            title: "Color Palette",
+            duration: 60,
+            videoUrl: "https://youtu.be/ixOd42SEUF0",
+          },
+          {
+            index: 5,
+            title: "Desain Respon",
+            duration: 60,
+            videoUrl: "https://youtu.be/DwTkyMJi890",
+          },
+        ],
+      },
+      {
+        title: "Chapter 1 - Pendahuluan",
+        totalDuration: 60,
+        videos: [
+          {
+            index: 1,
+            title: "Tujuan Mengikuti Kelas Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/ixOd42SEUF0",
+          },
+          {
+            index: 2,
+            title: "Pengenalan Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/DwTkyMJi890",
+          },
+          {
+            index: 3,
+            title: "Contoh Dalam Membangun Design System",
+            duration: 20,
+            videoUrl: "https://youtu.be/rd-590n3H6w",
+          },
+        ],
+      },
+      {
+        title: "Chapter 2 - Memulai Desain",
+        totalDuration: 120,
+        videos: [
+          {
+            index: 4,
+            title: "Color Palette",
+            duration: 60,
+            videoUrl: "https://youtu.be/ixOd42SEUF0",
+          },
+          {
+            index: 5,
+            title: "Desain Respon",
+            duration: 60,
+            videoUrl: "https://youtu.be/DwTkyMJi890",
+          },
+        ],
+      },
+    ];
+
+    let chapter1Course1;
+    let chapter2Course1;
+    let chapter1Course2;
+    let chapter2Course2;
+    let chapter1Course3;
+    let chapter2Course3;
+    
+    let categoryUiux;
+    let categoryProductManagement;
+    let categoryWebDevelopment;
+    let categoryAndroidDevelopment;
+    let categoryIOSDevelopment;
+    let categoryDataScience;
+    
     await User.insertMany(sampleUsers)
       .then((createdUsers) => {
         console.log("Berhasil membuat data pengguna:", createdUsers);
@@ -95,12 +240,121 @@ async function createDatabaseAndCollection() {
     await Category.insertMany(sampleCategory)
       .then((createdCategory) => {
         console.log("Berhasil membuat data category:", createdCategory);
+          categoryUiux = createdCategory[0]._id
+          categoryProductManagement = createdCategory[1]._id
+          categoryWebDevelopment = createdCategory[2]._id
+          categoryAndroidDevelopment = createdCategory[3]._id
+          categoryIOSDevelopment = createdCategory[4]._id
+          categoryDataScience = createdCategory[5]._id
       })
       .catch((error) => {
         console.error("Gagal membuat data category:", error);
       })
       .finally(() => {
         console.log("Insert category finish");
+      });
+
+      await Chapter.insertMany(sampleChapter)
+      .then((createdChapter) => {
+        console.log("Berhasil membuat data Chapter:", createdChapter);
+        chapter1Course1 = createdChapter[0]._id
+        chapter2Course1 = createdChapter[1]._id
+        chapter1Course2 = createdChapter[2]._id
+        chapter2Course2 = createdChapter[3]._id
+        chapter1Course3 = createdChapter[4]._id
+        chapter2Course3 = createdChapter[5]._id
+      })
+      .catch((error) => {
+        console.error("Gagal membuat data chapter:", error);
+      })
+      .finally(() => {
+        console.log("Insert chapter finish");
+      });
+
+      const sampleCourse = [
+        {
+          title:"Belajar Web Designer dengan Figma",
+          description:"     Design system adalah kumpulan komponen design, code, ataupun dokumentasi yang dapat digunakan sebagai panduan utama yang memunginkan designer serta developer memiliki lebih banyak kontrol atas berbagai platform. Dengan hadirnya design system, dapat menjaga konsistensi tampilan user interface dan meningkatkan user experience menjadi lebih baik. Disisi bisnis, design system sangat berguna dalam menghemat waktu dan biaya ketika mengembangkan suatu produk.",
+          thumbnail:"https://ik.imagekit.io/ku9epk6lrv/image_category_ui.png?updatedAt=1700640912911",
+          classCode:"P1000",
+          category:categoryUiux,
+          typeClass:"FREE",
+          level:"Beginner",
+          price:0,
+          "targetAudience": [
+              "1. Anda yang ingin memahami poin penting design system",
+                          "2. Anda yang ingin membantu perusahaan lebih optimal dalam membuat design produk",
+              "3. Anda yang ingin latihan membangun design system",
+              "4. Anda yang ingin latihan membangun design system "
+          ],
+          "totalModule": 2,
+          "totalRating":5.0,
+          "totalDuration:":180,
+          "chapters":[
+            chapter1Course1,
+            chapter2Course1,
+          ],
+          "sold": 0
+          },
+          {
+          title:"Menguasai Figma dengan Modern UI Dashboard Design",
+          description:"    Bersama mentor XXX, kita akan mempelajari design system dari mulai manfaat, alur kerja pembuatannya, tools yang digunakan, hingga pada akhirnya, kita akan membuat MVP dari design system. Selain itu, mentor juga akan menjelaskan berbagai resource yang dibutuhkan untuk mencari inspirasi mengenai design system.",
+          thumbnail:"https://ik.imagekit.io/ku9epk6lrv/image_category_ui.png?updatedAt=1700640912911",
+          classCode:"P2000",
+          category:categoryUiux,
+          typeClass:"FREE",
+          level:"Intermediate",
+          price:0,
+          "targetAudience": [
+              "1. Anda yang ingin memahami poin penting design system",
+                          "2. Anda yang ingin membantu perusahaan lebih optimal dalam membuat design produk",
+              "3. Anda yang ingin latihan membangun design system",
+              "4. Anda yang ingin latihan membangun design system "
+          ],
+          "totalModule": 2,
+          "totalRating":4.8,
+          "totalDuration:":180,
+          "chapters":[
+            chapter1Course2,
+            chapter2Course2,
+          ],
+          "sold": 0
+          },
+          {
+          title:"Membuat Grid System dengan Figma",
+          description:"     Kelas ini sesuai untuk Anda yang ingin memahami apa itu design system. Tidak hanya ditujukan untuk UI/UX Designer ataupun Developer, kelas ini sangat sesuai untuk stakeholder lain agar dapat memudahkan tim dalam bekerja sama. Yuk segera daftar dan kami tunggu di kelas ya!",
+          thumbnail:"https://ik.imagekit.io/ku9epk6lrv/image_category_ui.png?updatedAt=1700640912911",
+          classCode:"P3000",
+          category:categoryUiux,
+          typeClass:"FREE",
+          level:"Advanced",
+          price:0,
+          "targetAudience": [
+              "1. Anda yang ingin memahami poin penting design system",
+                          "2. Anda yang ingin membantu perusahaan lebih optimal dalam membuat design produk",
+              "3. Anda yang ingin latihan membangun design system",
+              "4. Anda yang ingin latihan membangun design system "
+          ],
+          "totalModule": 2,
+          "totalRating":5.0,
+          "totalDuration:":180,
+          "chapters":[
+            chapter1Course3,
+            chapter2Course3,
+          ],
+          "sold": 0
+          }
+      ]
+      
+      await Course.insertMany(sampleCourse)
+      .then((createdCourses) => {
+        console.log("Berhasil membuat data course:", createdCourses);
+      })
+      .catch((error) => {
+        console.error("Gagal membuat data course:", error);
+      })
+      .finally(() => {
+        console.log("Insert course finish");
       });
 
     client.disconnect();
