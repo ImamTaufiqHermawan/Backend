c234a776fafa2b601e4963d6b3c15e69368fa909
irfiyandaabidin <irfidupang@gmail.com>
2023-12-11 23:49:34 +0700

    feature/api-statistik

--- app/routes/category.js
+++ app/routes/category.js
@@ -4,5 +4,6 @@ const categoryController = require('../controllers/category');
 route.get('/', categoryController.getCategoryCourse);
 route.get('/progress', categoryController.getCategoryProgress);
 route.get('/type-class', categoryController.getCategoryTypeClass);
+route.get('/statistik', categoryController.getStatistik);
 
 module.exports = route;
