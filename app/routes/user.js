9e197ca5f26b2f6ebff737c2dd12f3383d6da105
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2023-12-27 16:21:59 +0700

    add api delete user create user for admin

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,7 @@
 const route = require('express').Router();
 const Authenthicate = require('../middleware/authenticate');
 const UserContorller = require('../controllers/user');
+const Authorize = require('../middleware/authorization');
 const multer = require('multer');
 const uploadImage = require('../helpers/uploadImage');
 const validationId = require('../middleware/validation');
@@ -10,6 +11,8 @@ const upload = multer({storage: storage});
 
 route.patch('/update-password/:id', Authenthicate, validationId('User'), UserContorller.updatePassword);
 route.get('/', Authenthicate, UserContorller.getAllUsers);
+route.post('/create-user', Authenthicate, Authorize('admin'), UserContorller.addUser);
+route.delete('/:id', Authenthicate, Authorize('admin'), UserContorller.deleteUser);
 route.get('/:id', Authenthicate, validationId('User'), UserContorller.getUserById);
 route.patch('/:id', Authenthicate, validationId('User'), upload.single('imageProfile'), uploadImage, UserContorller.updateUser);
 
