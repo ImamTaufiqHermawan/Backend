dbe7e7953b07948592e4bb969a7e3c3ae2a8d29e
irfiyandaabidin <irfidupang@gmail.com>
2023-12-27 18:02:18 +0700

    add response chart API statistik

--- app/controllers/category.js
+++ app/controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require('../models/category');
+const Transaction = require('../models/transaction');
 const Course = require('../models/course');
 const User = require('../models/user');
 const ApiError = require('../utils/apiError');
@@ -50,16 +51,34 @@ const getCategoryTypeClass = (req, res) => {
       .send(resSuccess('Get all category type class successfully', data));
 };
 
-const getStatistik = async (req, res) => {
+const getStatistik = async (req, res, next) => {
   try {
     const usersAll = await User.find({isActive: true, role: 'user'});
     const classPremium = await Course
         .find({isActive: true, typeClass: 'PREMIUM'});
     const classAll = await Course.find({isActive: true});
+    const dateNow = new Date();
+    const oneMonthAgo = new Date().setMonth(dateNow.getMonth() - 1);
+    const chartUser = [];
+    usersAll.map((user) => {
+      if (user.createdAt >= oneMonthAgo) {
+        chartUser.push(user);
+      };
+    });
+    const transactionAll = await Transaction
+        .find({isActive: true, status: 'Paid'});
+    const chartTransaction = [];
+    transactionAll.map((transaction) => {
+      if (transaction.createdAt >= oneMonthAgo) {
+        chartTransaction.push(transaction);
+      };
+    });
     const data = {
       activeUsers: usersAll.length,
       premiumClass: classPremium.length,
       activeClass: classAll.length,
+      chartUser,
+      chartTransaction,
     };
     res.status(200)
         .send(resSuccess('Get statistik successfully', data));
